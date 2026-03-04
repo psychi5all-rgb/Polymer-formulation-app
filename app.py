927a8a3bb7845b7a997829be4a581e2df859fa04
psychi5all-rgb <psychi5.all@gmail.com>
2026-03-04 17:57:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+st.set_page_config(layout="wide")
 # ==========================================================
 # PAGE CONFIG 🧪
 # ==========================================================
